f8f497320edf0742066aa93a6551c8bd6311516f
Amanuel-web <73563501+Amanuel-web@users.noreply.github.com>
2023-03-01 17:02:26 -0800

    Project name changed

--- src/test/java/com/Vytruck/pages/BasePage.java
+++ src/test/java/com/Vytruck/pages/BasePage.java
@@ -1,10 +1,10 @@
-package com.project_name.pages;
+package com.Vytruck.pages;
 
 
 
 
-import com.project_name.utilities.BrowserUtils;
-import com.project_name.utilities.Driver;
+import com.Vytruck.utilities.BrowserUtils;
+import com.Vytruck.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
--- src/test/java/com/Vytruck/utilities/Driver.java
+++ src/test/java/com/Vytruck/utilities/Driver.java
@@ -1,4 +1,4 @@
-package com.project_name.utilities;
+package com.Vytruck.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
